83d69e848db1f275c4c56af722e5600c76a43a68
Thiago Pereira <thiagopereiramail@gmail.com>
2022-04-12 16:50:06 +0000

    Create styles on the first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 export default function Main() {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e título: React Avançado"
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor editando código"
+      />
+    </S.Wrapper>
   )
 }
