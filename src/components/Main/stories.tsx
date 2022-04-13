3480fc731846c0ed2c9da4e53bf214800ec6a53d
Thiago Pereira <thiagopereiramail@gmail.com>
2022-04-13 11:24:21 +0000

    Pass arguments for a story

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -3,7 +3,18 @@ import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  args: {
+    title: 'Título padrão',
+    description: 'Descrição padrão'
+  }
 } as ComponentMeta<typeof Main>
 
 export const Basic: ComponentStory<typeof Main> = (args) => <Main {...args} />
+
+Basic.args = {
+  title: 'Título básico',
+  description: 'Descrição básica'
+}
+
+export const Default: ComponentStory<typeof Main> = (args) => <Main {...args} />
